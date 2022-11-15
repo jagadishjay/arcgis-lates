d3e52da53638e6abba0ceb633dc08d9afac78e78
jagadishjay~ <jagadishbabu95@gmail.com>
2022-11-15 13:33:31 +0530

    widgets added

--- src/app/arcgis/arcgis.component.ts
+++ src/app/arcgis/arcgis.component.ts
@@ -45,46 +45,80 @@ export class ArcgisComponent implements OnInit {
   async initializeMap() {
     try {
       
-      const [EsriMap, EsriMapView,SceneView, BasemapGallery] = await loadModules([
-        'esri/Map',
-        "esri/views/SceneView",  'esri/views/MapView',"esri/widgets/BasemapGallery"
+      // const [EsriMap, EsriMapView,SceneView, BasemapGallery] = await loadModules([
+      //   'esri/Map',
+      //   "esri/views/SceneView",  'esri/views/MapView',"esri/widgets/BasemapGallery"
+      // ]);
+      const [Map, Locate,CoordinateConversion, SceneView,Expand, BasemapGallery] = await loadModules([
+        "esri/Map", "esri/widgets/Locate", "esri/widgets/CoordinateConversion",
+        "esri/views/SceneView", "esri/widgets/Expand",
+        "esri/widgets/BasemapGallery",  "esri/WebMap"
       ]);
-
       // Set type of map
       const mapProperties: esri.MapProperties = {
         basemap: this._basemap
       };
 
-      const map: esri.Map = new EsriMap(mapProperties);
-      
+     // const map: esri.Map = new EsriMap(mapProperties);
+      const map1 = new Map({
+        basemap: "gray-vector"
+      });
 
       // Set type of map view
       const mapViewProperties: esri.MapViewProperties = {
         container: "viewDiv",
         center: this._center,
         zoom: this._zoom,
-        map: map
+        map: map1
       };
-
-      const mapView: esri.MapView = new EsriMapView(mapViewProperties);
+     
+      //const mapView: esri.MapView = new EsriMapView(mapViewProperties);
 
       const view = new SceneView({
         container: "viewDiv",
-        map: map,
+        map: map1,
         center: [139.68, 35.68],
         zoom: 3
       });
       const basemapGallery = new BasemapGallery({
-        view: view
+        view: view,
+        container: document.createElement("div")
       });
-      view.ui.add(basemapGallery, {
+      const bgExpand = new Expand({
+        view: view,
+        content: basemapGallery
+      });
+     // close the expand whenever a basemap is selected
+     basemapGallery.watch("activeBasemap", () => {
+      const mobileSize =
+        view.heightBreakpoint === "xsmall" ||
+        view.widthBreakpoint === "xsmall";
+
+      if (mobileSize) {
+        bgExpand.collapse();
+      }
+    });
+    
+      view.ui.add(bgExpand, {
         position: "top-right"
       });
+      const ccWidget = new CoordinateConversion({
+        view: view
+      });
+
+      view.ui.add(ccWidget, "bottom-left");
  
-  
+      const locateBtn = new Locate({
+        view: view
+      });
+
+      // Add the locate widget to the top left corner of the view
+      view.ui.add(locateBtn, {
+        position: "top-left"
+      });
       // All resources in the MapView and the map have loaded.
       // Now execute additional processes
-      mapView.when(() => {
+      view.when(() => {
         this.mapLoaded.emit(true);
       });
     } catch (error) {
